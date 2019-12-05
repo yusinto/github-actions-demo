7f4089ce9c9cf88ae5ac6c61307985ad847419cc
Yusinto Ngadiman <yusinto@gmail.com>
2019-12-05 09:19:00 -0800

    Added lastname/surname bug

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -4,7 +4,7 @@ import App from './app'
 
 describe('app', () => {
   test('renders root app component correctly', () => {
-    const { asFragment } = render(<App firstName="Wendy" />)
+    const { asFragment } = render(<App firstName="Yus" />)
     expect(asFragment()).toMatchSnapshot()
   })
 })
--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,14 @@ import React, { FunctionComponent, useState } from 'react'
 
 interface AppProps {
   firstName: string
+  lastName?: string
 }
-const App: FunctionComponent<AppProps> = ({ firstName }) => {
-  return <div>Welcome github actions {firstName}!</div>
+const App: FunctionComponent<AppProps> = ({ firstName, surname }) => {
+  return (
+    <div>
+      Welcome github actions {firstName} {surname}!
+    </div>
+  )
 }
 
 export default App
